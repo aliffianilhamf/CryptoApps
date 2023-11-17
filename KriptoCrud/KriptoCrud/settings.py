1da79ad8e473f30f0feb6343bf5c9523c3d0c288
aliffianilhamf <aliffianilhamf@gmail.com>
2023-11-17 07:27:13 +0700

    fitur login, sign in, logout done

--- KriptoCrud/KriptoCrud/settings.py
+++ KriptoCrud/KriptoCrud/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+import os
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -39,7 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'webapp',
+    'crispy_forms',
+    'crispy_bootstrap4',
+   
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -120,9 +127,13 @@ USE_TZ = True
 STATIC_URL = 'static/'
 
 
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR / 'static')
+                    
+]
 
-
+MEDIA_URL = 'static/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
--- KriptoCrud/webapp/views.py
+++ KriptoCrud/webapp/views.py
@@ -1,6 +1,56 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import CreateUser, Login
+
+from django.contrib.auth.models import auth
+from django.contrib.auth import authenticate
 
 # Create your views here.
 def index(request):
     return render(request, 'webapp/index.html')
+
+# register
+def register(request) :
+    form = CreateUser()
+    
+    if request.method == "POST" :
+        form = CreateUser(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+           
+    context = {
+        'form' : form
+    }
+    return render(request, 'webapp/register.html', context)
+        
+# login a user
+def login(request):
+    form = Login()
+    if request.method == "POST":
+        form = Login(request, data=request.POST)
+        if form.is_valid():
+            username = request.POST.get("username")
+            password = request.POST.get("password")
+            
+            user = authenticate(request, username = username, password = password)
+            if user is not None:
+                auth.login(request, user)
+                
+                return redirect("index")
+    context = {
+        'form' : form
+    }
+    
+    return render(request, 'webapp/login.html', context)
+
+# dashboard
+def dashboard(request):
+    
+    return render(request, 'webapp/index.html')
+
+
+# logout
+def logout(request):
+    auth.logout(request)
+    
+    return redirect("login")
